998f661c1c03ac5c224c8add06c432cfcd1cfeb7
Dev Pedro Azevedo <pedroazevedoteixeira7@gmail.com>
2024-04-12 08:10:15 -0300

    .

--- src/components/GoodForm.jsx
+++ src/components/GoodForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { isEmail } from "validator";
 import Logo from "../assets/logo.png";
